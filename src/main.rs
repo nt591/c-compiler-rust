617ea87ecd6eeb180063afba85e66cc713b66019
Nikhil Thomas <nikhil@nthomas.org>
2024-09-01 15:06:09 -0400

    scaffold to run CLI

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[clap(name = "C Compiler")]
+#[clap(author = "Nikhil Thomas")]
+struct Args {
+    input: PathBuf,
+
+    /// Run lexical analysis
+    #[arg(long, conflicts_with_all = ["parse", "codegen"])]
+    lex: bool,
+
+    /// Run parsing
+    #[arg(long, conflicts_with_all = ["lex", "codegen"])]
+    parse: bool,
+
+    /// Run code generation
+    #[arg(long, conflicts_with_all = ["lex", "parse"])]
+    codegen: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+    let _input = args.input.canonicalize().expect("Not a valid input file");
+    if args.lex {
+        println!("lexing");
+    } else if args.parse {
+        println!("parsing");
+    } else if args.codegen {
+        println!("codegen");
+    } else {
+        println!("full run");
+    }
+    ()
+}
