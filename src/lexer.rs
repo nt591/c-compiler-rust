878c34b141ccc137289f3a554abc3a61f95a3823
Nikhil Thomas <nikhil@nthomas.org>
2024-09-28 15:06:41 -0400

    add tests for source from ch1

--- src/lexer.rs
+++ src/lexer.rs
@@ -7,10 +7,12 @@ pub enum LexerError {
 }
 
 // todo: let's stop carrying the entire text around
+#[derive(Debug)]
 pub struct Lexer<'a> {
     tokens: Vec<Token<'a>>,
 }
 
+#[derive(Debug, PartialEq)]
 pub enum Token<'a> {
     Identifier(&'a str),
     Constant(usize),
@@ -26,85 +28,133 @@ pub enum Token<'a> {
     Semicolon,
 }
 
-pub fn lex<'a>(source: &'a str) -> Result<Lexer<'a>, LexerError> {
-    let mut tokens = Vec::new();
-    let mut idx = 0;
-    let len = source.len();
-    // todo: I guess emojis will blow up here
-    let bytes = source.as_bytes();
-    while idx < len {
-        let c = bytes[idx];
-        match c {
-            c if c.is_ascii_whitespace() => (),
-            b'{' => tokens.push(Token::LeftBrace),
-            b'}' => tokens.push(Token::RightBrace),
-            b'(' => tokens.push(Token::LeftParen),
-            b')' => tokens.push(Token::RightParen),
-            b';' => tokens.push(Token::Semicolon),
-            b'a'..=b'z' | b'A'..=b'Z' => {
-                // starts with a letter, just walk until the end.
-                let mut end = idx;
-                while end < len && is_valid_identifier_character(bytes[end]) {
-                    end += 1;
-                }
-
-                // scoop up whatever's in the range, 
-                // check for known keywords, else just
-                // return an identifier? Or just ignore.
-                if let Some(keyword_token) = parse_keyword(&source[idx..end]) {
-                    tokens.push(keyword_token);
-                } else {
-                    todo!();
-                }
-            }
-            c if c.is_ascii_digit() => {
-                // advance while we see digits
-                let mut end = idx;
-                while end < len && bytes[end].is_ascii_digit() {
-                    end += 1;
-                }
-                // at this point, end is either EOF
-                // or non-digit character. For now, just error
-                // if end points to a..z or A..Z.
-                // This does not support floats!
-                if end == len {
-                    break;
-                }
-                if bytes[end].is_ascii_alphabetic() {
-                    return Err(LexerError::UnexpectedToken(
-                        String::from_utf8(bytes[idx..=end].to_vec()).expect(
-                            "Got invalid UTF-8 when addressing byte range from source string",
-                        ),
-                    ));
-                }
-
-                // todo: is this the best I can do?
-                let s =
-                    std::str::from_utf8(&bytes[idx..end]).expect("We know this is UTF8");
-                let constant: usize = s
-                    .parse()
-                    .expect("Didn't get a digit after parsing a string");
-                tokens.push(Token::Constant(constant));
-            }
-            _ => todo!(),
-        }
-        idx += 1;
+impl<'a> Lexer<'a> {
+    pub fn tokens(&self) -> std::slice::Iter<'a, Token> {
+        self.tokens.iter()
     }
 
-    Ok(Lexer { tokens })
+    pub fn lex(source: &'a str) -> Result<Lexer<'a>, LexerError> {
+        let mut tokens = Vec::new();
+        let mut idx = 0;
+        let len = source.len();
+        // todo: I guess emojis will blow up here
+        let bytes = source.as_bytes();
+        while idx < len {
+            let c = bytes[idx];
+            match c {
+                c if c.is_ascii_whitespace() => (),
+                b'{' => tokens.push(Token::LeftBrace),
+                b'}' => tokens.push(Token::RightBrace),
+                b'(' => tokens.push(Token::LeftParen),
+                b')' => tokens.push(Token::RightParen),
+                b';' => tokens.push(Token::Semicolon),
+                b'a'..=b'z' | b'A'..=b'Z' => {
+                    // starts with a letter, just walk until the end.
+                    let start = idx;
+                    let mut end = idx;
+                    while end < len && Self::is_valid_identifier_character(bytes[end]) {
+                        end += 1;
+                    }
+
+                    // scoop up whatever's in the range,
+                    // check for known keywords, else just
+                    // return an identifier? Or just ignore.
+                    if let Some(keyword_token) = Self::parse_keyword(&source[start..end]) {
+                        tokens.push(keyword_token);
+                    } else {
+                        todo!();
+                    }
+                    idx = end - 1; //since we're incrementing at the end
+                }
+                c if c.is_ascii_digit() => {
+                    // advance while we see digits
+                    let start = idx;
+                    let mut end = idx;
+
+                    while end < len && bytes[end].is_ascii_digit() {
+                        end += 1;
+                    }
+                    // If end == len, we parsed the last character. In that
+                    // case, let's just check that if idx < len, the next character
+                    // is not an alphabetic character.
+                    // This does not support floats!
+                    if end < len && bytes[end].is_ascii_alphabetic() {
+                        return Err(LexerError::UnexpectedToken(
+                            String::from_utf8(bytes[start..=idx].to_vec()).expect(
+                                "Got invalid UTF-8 when addressing byte range from source string",
+                            ),
+                        ));
+                    }
+
+                    // todo: is this the best I can do?
+                    let s = std::str::from_utf8(&bytes[start..end]).expect("We know this is UTF8");
+                    let constant: usize = s
+                        .parse()
+                        .expect("Didn't get a digit after parsing a string");
+                    tokens.push(Token::Constant(constant));
+                    idx = end - 1;
+                }
+                _ => todo!(),
+            }
+            idx += 1;
+        }
+
+        Ok(Lexer { tokens })
+    }
+
+    fn is_valid_identifier_character(c: u8) -> bool {
+        matches!(c, b'a'..=b'z' | b'A'..=b'Z'| b'0'..=b'9')
+    }
+
+    fn parse_keyword(s: &str) -> Option<Token> {
+        match s {
+            "int" => Some(Token::Int),
+            "main" => Some(Token::Main),
+            "return" => Some(Token::Return),
+            "void" => Some(Token::Void),
+            _ => None,
+        }
+    }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-fn is_valid_identifier_character(c: u8) -> bool {
-    matches!(c, b'a'..=b'z' | b'A'..=b'Z'|b'0'..=b'9')
-}
+    #[test]
+    fn basic_keywords() {
+        let source = "int main 123;";
+        let lexer = Lexer::lex(source);
+        assert!(lexer.is_ok());
+        let lexer = lexer.unwrap();
+        let mut tokens = lexer.tokens();
+        assert_eq!(Some(&Token::Int), tokens.next());
+        assert_eq!(Some(&Token::Main), tokens.next());
+        assert_eq!(Some(&Token::Constant(123)), tokens.next());
+        assert_eq!(Some(&Token::Semicolon), tokens.next());
+        assert_eq!(None, tokens.next());
+    }
 
-fn parse_keyword(s: &str) -> Option<Token> {
-    match s {
-        "int" => Some(Token::Int),
-        "main" => Some(Token::Main),
-        "return" => Some(Token::Return),
-        "void" => Some(Token::Void),
-        _ => None
+    #[test]
+    fn basic_source() {
+        let source = r#"
+        int main(void) {
+            return 2;
+        }"#;
+        let lexer = Lexer::lex(source);
+        assert!(lexer.is_ok());
+        let lexer = lexer.unwrap();
+        let mut tokens = lexer.tokens();
+        assert_eq!(Some(&Token::Int), tokens.next());
+        assert_eq!(Some(&Token::Main), tokens.next());
+        assert_eq!(Some(&Token::LeftParen), tokens.next());
+        assert_eq!(Some(&Token::Void), tokens.next());
+        assert_eq!(Some(&Token::RightParen), tokens.next());
+        assert_eq!(Some(&Token::LeftBrace), tokens.next());
+        assert_eq!(Some(&Token::Return), tokens.next());
+        assert_eq!(Some(&Token::Constant(2)), tokens.next());
+        assert_eq!(Some(&Token::Semicolon), tokens.next());
+        assert_eq!(Some(&Token::RightBrace), tokens.next());
+        assert_eq!(None, tokens.next());
     }
 }
