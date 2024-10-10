36d1c409fad9a4511d0fa1d4da86dbe6785a0fa1
Nikhil Thomas <nikhil@nthomas.org>
2024-10-10 19:17:34 -0400

    ch2: codegen and fixups work

--- src/asm.rs
+++ src/asm.rs
@@ -2,17 +2,6 @@
 // and converting to an assembly AST
 use crate::tacky;
 use std::collections::HashMap;
-use thiserror::Error;
-
-#[derive(Debug, PartialEq, Error)]
-pub enum AsmError {
-    #[error("Expected program")]
-    MissingProgram,
-    #[error("Expected function")]
-    MissingFunction,
-    #[error("Invalid Instruction")]
-    InvalidInstruction,
-}
 
 // Lifetime of source test, since we need
 // names. TODO: Figure out how to remove this dep.
@@ -67,8 +56,7 @@ impl<'a> Asm<'a> {
     pub fn from_tacky(tacky: tacky::AST<'a>) -> Asm<'a> {
         let mut asm = Self::parse_program(&tacky);
         let mut generator = AsmGenerator::default();
-        Self::fixup_pseudos(&mut asm, &mut generator);
-        Self::insert_allocate_stack(&mut asm, &generator);
+        Self::fixup(&mut asm, &mut generator);
         asm
     }
 
@@ -102,17 +90,19 @@ impl<'a> Asm<'a> {
             .collect::<Vec<_>>()
     }
 
-    fn fixup_pseudos(asm: &mut Asm<'a>, gen: &mut AsmGenerator) {
+    fn fixup(asm: &mut Asm<'a>, gen: &mut AsmGenerator) {
         match asm {
-            Asm::Program(ref mut func) => Self::fixup_pseudos_in_function(func, gen),
+            Asm::Program(ref mut func) => Self::fixup_function(func, gen),
         };
     }
-    fn fixup_pseudos_in_function(func: &mut Function<'a>, gen: &mut AsmGenerator) {
+    fn fixup_function(func: &mut Function<'a>, gen: &mut AsmGenerator) {
         let Function {
             name: _name,
             ref mut instructions,
         } = func;
         Self::fixup_pseudos_in_instructions(instructions, gen);
+        Self::insert_alloc_stack_func(func, gen);
+        Self::fixup_invalid_memory_accesses(func);
     }
 
     fn fixup_pseudos_in_instructions(ins: &mut [Instruction], gen: &mut AsmGenerator) {
@@ -143,24 +133,34 @@ impl<'a> Asm<'a> {
         }
     }
 
-    fn insert_allocate_stack(asm: &mut Asm<'a>, gen: &AsmGenerator) {
-        match asm {
-            Asm::Program(ref mut func) => Self::insert_alloc_stack_func(func, gen),
-        };
-    }
-
     fn insert_alloc_stack_func(func: &mut Function<'a>, gen: &AsmGenerator) {
         let old_ins = std::mem::take(&mut func.instructions);
-        let new_instructions = Self::insert_alloc_stack_ins(old_ins, gen);
-        func.instructions = new_instructions;
-    }
-
-    fn insert_alloc_stack_ins(ins: Vec<Instruction>, gen: &AsmGenerator) -> Vec<Instruction> {
         let mut v = vec![Instruction::AllocateStack(gen.stack_offset)];
-        for i in ins.into_iter() {
+        for i in old_ins.into_iter() {
             v.push(i);
         }
-        v
+        func.instructions = v;
+    }
+
+    fn fixup_invalid_memory_accesses(func: &mut Function<'a>) {
+        let old_ins = std::mem::take(&mut func.instructions);
+        let mut v = vec![];
+        for ins in old_ins.into_iter() {
+            match ins {
+                Instruction::Mov(Operand::Stack(src), Operand::Stack(dst)) => {
+                    v.push(Instruction::Mov(
+                        Operand::Stack(src),
+                        Operand::Reg(Register::R10),
+                    ));
+                    v.push(Instruction::Mov(
+                        Operand::Reg(Register::R10),
+                        Operand::Stack(dst),
+                    ));
+                }
+                i => v.push(i),
+            }
+        }
+        func.instructions = v;
     }
 }
 
@@ -209,6 +209,51 @@ mod tests {
                 Instruction::Mov(Operand::Imm(100), Operand::Stack(-4)),
                 Instruction::Unary(UnaryOp::Neg, Operand::Stack(-4)),
                 Instruction::Mov(Operand::Stack(-4), Operand::Reg(Register::EAX)),
+                Instruction::Ret,
+            ],
+        });
+
+        let assembly = Asm::from_tacky(ast);
+        assert_eq!(assembly, expected);
+    }
+
+    #[test]
+    fn parse_nested_unaries() {
+        let ast = tacky::AST::Program(tacky::Function {
+            name: "main",
+            instructions: vec![
+                tacky::Instruction::Unary {
+                    op: tacky::UnaryOp::Negate,
+                    src: tacky::Val::Constant(100),
+                    dst: tacky::Val::Var("tmp.0".into()),
+                },
+                tacky::Instruction::Unary {
+                    op: tacky::UnaryOp::Complement,
+                    src: tacky::Val::Var("tmp.0".into()),
+                    dst: tacky::Val::Var("tmp.1".into()),
+                },
+                tacky::Instruction::Unary {
+                    op: tacky::UnaryOp::Negate,
+                    src: tacky::Val::Var("tmp.1".into()),
+                    dst: tacky::Val::Var("tmp.2".into()),
+                },
+                tacky::Instruction::Ret(tacky::Val::Var("tmp.2".into())),
+            ],
+        });
+
+        let expected = Asm::Program(Function {
+            name: "main",
+            instructions: vec![
+                Instruction::AllocateStack(-12),
+                Instruction::Mov(Operand::Imm(100), Operand::Stack(-4)),
+                Instruction::Unary(UnaryOp::Neg, Operand::Stack(-4)),
+                Instruction::Mov(Operand::Stack(-4), Operand::Reg(Register::R10)),
+                Instruction::Mov(Operand::Reg(Register::R10), Operand::Stack(-8)),
+                Instruction::Unary(UnaryOp::Not, Operand::Stack(-8)),
+                Instruction::Mov(Operand::Stack(-8), Operand::Reg(Register::R10)),
+                Instruction::Mov(Operand::Reg(Register::R10), Operand::Stack(-12)),
+                Instruction::Unary(UnaryOp::Neg, Operand::Stack(-12)),
+                Instruction::Mov(Operand::Stack(-12), Operand::Reg(Register::EAX)),
                 Instruction::Ret,
             ],
         });
